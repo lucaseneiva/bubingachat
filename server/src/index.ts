ef1bf1c3270bb1ccda884897d9d4797113032e8b
Lucas E. Neiva <neiva.lucas13@gmail.com>
2026-02-10 10:08:45 -0300

    feat: create docker compose for database, database connection in server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,21 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/db.js';
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+
 const port = 3000;
+
+dotenv.config();
 
 app.get("/", (req, res) => {
     res.send("hello world");
 })
+
+connectDB();
 
 app.listen(port, () => {
     console.log(`server running in http://localhost:${port}`);
